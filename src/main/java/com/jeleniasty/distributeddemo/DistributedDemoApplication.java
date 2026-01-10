45396a7d9b6b11f3bae3a556ef2fc72c175215a4
micha <michal.jeleniasty@gmail.com>
2026-01-10 17:05:53 +0100

    Initial commit

---
+++ src/main/java/com/jeleniasty/distributeddemo/DistributedDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.distributeddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedDemoApplication.class, args);
+    }
+
+}
