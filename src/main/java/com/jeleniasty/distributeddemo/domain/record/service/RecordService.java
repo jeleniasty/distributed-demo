30fec6263638462ffb4371ff801f95c9f0eeb860
micha <michal.jeleniasty@gmail.com>
2026-01-12 17:23:31 +0100

    added redis cache

--- src/main/java/com/jeleniasty/distributeddemo/domain/record/service/RecordService.java
+++ src/main/java/com/jeleniasty/distributeddemo/domain/record/service/RecordService.java
@@ -8,6 +8,8 @@ import com.jeleniasty.distributeddemo.domain.record.entity.Record;
 import com.jeleniasty.distributeddemo.domain.record.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,10 @@ public class RecordService {
 
     private final RecordRepository recordRepository;
 
+    @CacheEvict(
+            value = "record-pages",
+            allEntries = true
+    )
     @Transactional
     public void createRecord(CreateRecordDto dto) {
         log.info("Creating new record with data: {}", dto);
@@ -27,6 +33,10 @@ public class RecordService {
         log.info("Record [id: {}] has been created", record.getId());
     }
 
+    @Cacheable(
+            value = "records",
+            key = "#id"
+    )
     @Transactional(readOnly = true)
     public RecordDetailsDto getRecord(Long id) {
         log.info("Getting record with id: {}", id);
@@ -39,6 +49,10 @@ public class RecordService {
                 .orElseThrow(() -> RecordException.notFound(id));
     }
 
+    @Cacheable(
+            value = "record-pages",
+            key = "'page=' + #pageable.pageNumber + ',size=' + #pageable.pageSize"
+    )
     @Transactional(readOnly = true)
     public Page<RecordDto> getRecords(Pageable pageable) {
         log.info("Getting records... [page {}, size {}]", pageable.getPageNumber(), pageable.getPageSize());
